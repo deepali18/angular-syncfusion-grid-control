866cda76f1af11fe8223f5f899f2053c8d4b126c
deepali18 <deepali.imagine@gmail.com>
2020-10-19 17:10:57 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+import { PageService, SortService, FilterService, GroupService } from '@syncfusion/ej2-angular-grids';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { GridModule } from '@syncfusion/ej2-angular-grids';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, GridModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers:    [PageService,
+                SortService,
+                FilterService,
+                GroupService]
 })
 export class AppModule { }
